3b5c4593c6ddd6333f6448369053f0af4e3870bf
imparang <jeongjaeyn@gmail.com>
2021-12-10 15:07:44 +0900

    feat: Add a auto completed search query  and fetch searching results logic in SearchForm component

--- front/src/components/SearchProduct.js
+++ front/src/components/SearchProduct.js
@@ -1,19 +1,84 @@
+import axios from 'axios'
 import React, { useCallback, useState } from 'react'
-import { Form, FormGroup, Input, Label } from 'reactstrap'
+import {
+  Form,
+  FormGroup,
+  Input,
+  Label,
+  ListGroup,
+  ListGroupItem
+} from 'reactstrap'
 
-const SearchProduct = () => {
+const SearchProduct = ({ getData }) => {
   const [query, setQuery] = useState('')
+  const [autoCompletedQuery, setAutoCompletedQuery] = useState([])
+  const [searchResults, setSearchResults] = useState([])
 
-  const onChangeQuery = useCallback(e => {
-    setQuery(e.target.value)
+  const autoCompletedSearchQuery = useCallback(async query => {
+    try {
+      const res = await axios.get(`/shop?query=${query}`)
+      if (res && res.status === 200) {
+        const { data } = res
+        setAutoCompletedQuery(data.items)
+      }
+    } catch (e) {
+      console.log('error ', e)
+    }
   }, [])
 
+  const fetchSearchResult = useCallback(async query => {
+    try {
+      const res = await axios.get(`/shop/search?query=${query}`)
+      if (res && res.status === 200) {
+        const { data } = res
+        console.log(data.items)
+        setSearchResults(data.items)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }, [])
+
+  const onChangeQuery = useCallback(
+    text => {
+      setQuery(text)
+      autoCompletedSearchQuery(query)
+    },
+    [query, autoCompletedSearchQuery]
+  )
+
+  const onSearch = useCallback(
+    e => {
+      e.preventDefault()
+      fetchSearchResult(query)
+      setAutoCompletedQuery([])
+      console.log(searchResults)
+      getData(searchResults)
+    },
+    [query, fetchSearchResult, searchResults, getData]
+  )
   return (
-    <Form style={{ marginBottom: '40px' }}>
+    <Form style={{ marginBottom: '40px' }} onSubmit={onSearch}>
       <FormGroup>
         <Label>쇼핑 품목을 검색하세요</Label>
-        <Input type="text" value={query} onChange={onChangeQuery} autoFocus />
+        <Input
+          type="text"
+          value={query}
+          onChange={e => {
+            onChangeQuery(e.target.value)
+          }}
+          autoFocus
+        />
       </FormGroup>
+      {query && (
+        <ListGroup>
+          {autoCompletedQuery.map(item =>
+            item.map(el => (
+              <ListGroupItem key={Math.random()}>{el}</ListGroupItem>
+            ))
+          )}
+        </ListGroup>
+      )}
     </Form>
   )
 }
