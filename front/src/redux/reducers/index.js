cf703a3d60625db7a0d5b680960b513e1a873703
imparang <jeongjaeyn@gmail.com>
2021-12-14 00:30:02 +0900

    fix: Remove redux files: not use

--- front/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'
-import { queryReducer, marketReducer } from './shop'
-const rootReducer = combineReducers({
-  query: queryReducer,
-  market: marketReducer
-})
-
-export default rootReducer
--- front/src/redux/sagas/index.js
+++
@@ -1,5 +0,0 @@
-import { all, fork } from 'redux-saga'
-
-export default function* rootSaga() {
-  yield all([fork()])
-}
